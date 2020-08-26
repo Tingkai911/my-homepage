c6fe0745dbea513e4dd57355037e58b1f9137119
TingKai911 <tingkai911@gmail.com>
2020-08-26 13:13:21 +0800

    manifest.json changes

---
+++ build/precache-manifest.7d0c779a6c97e5ebf0ab2e11dc19dc3f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e717ee39ff817138e30ad7b28e4fc6db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb4a0517fa69207621a",
+    "url": "/static/css/main.8a86649b.chunk.css"
+  },
+  {
+    "revision": "b83c077fff88e9061862",
+    "url": "/static/js/2.fd8da299.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd8da299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb4a0517fa69207621a",
+    "url": "/static/js/main.566dec0d.chunk.js"
+  },
+  {
+    "revision": "2a43383115fdba64099c",
+    "url": "/static/js/runtime-main.36cdd962.js"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/static/media/black-header-background.efdba046.jpg"
+  },
+  {
+    "revision": "9ccd703cd302c041fbc8693a9c87208c",
+    "url": "/static/media/blue-header-background.9ccd703c.jpg"
+  },
+  {
+    "revision": "0274588e31ace5185a862f1d02c01f7a",
+    "url": "/static/media/cs50blog.0274588e.jpg"
+  },
+  {
+    "revision": "5149a3610df80f8df43b5138ebe85010",
+    "url": "/static/media/cs50blog2.5149a361.jpg"
+  },
+  {
+    "revision": "fc726e73276c044b5c8aad86864e8692",
+    "url": "/static/media/follow.fc726e73.jpg"
+  },
+  {
+    "revision": "a9ee2a5bdadecfa6248a9106158b803f",
+    "url": "/static/media/githubicon.a9ee2a5b.png"
+  },
+  {
+    "revision": "981dc92c5d105c1044faf9d8dbfabaf5",
+    "url": "/static/media/memegenerator.981dc92c.jpg"
+  },
+  {
+    "revision": "141ab7fbc6dc7a118f2d61bbbe51ced1",
+    "url": "/static/media/profilepic.141ab7fb.png"
+  },
+  {
+    "revision": "f5a3fb31ebb82a372757e6b50aab7002",
+    "url": "/static/media/purple-header-background.f5a3fb31.jpg"
+  },
+  {
+    "revision": "2e3e451e885dbc3f7cbd408e5aafddc1",
+    "url": "/static/media/resume.2e3e451e.pdf"
+  },
+  {
+    "revision": "3747677398e001f46349ffd35dc00952",
+    "url": "/static/media/white-header-background.37476773.jpg"
+  }
+]);
